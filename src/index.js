7122ea1bece25b90135b02e917a4a771c5acdcfa
Jascha <jascha@jaeh.at>
2019-05-07 06:58:02 +0200

    fix target

--- src/index.js
+++ src/index.js
@@ -3,13 +3,8 @@ const theme = {
     maxWidth: '1300px',
   },
 
-  'h1, h2, h3': {
+  'h1, h2, h3, h4, h5': {
     '&:target': {
-      textDecoration: 'underline',
-    },
-  },
-  ':target': {
-    'h1, h2, h3': {
       textDecoration: 'underline',
     },
   },
