23d5fe3881facd9bfec9f89b9c9e25ec8f8f92c9
Jascha <jascha@jaeh.at>
2019-06-01 18:20:28 +0200

    theme exports a function now

--- test/spec.mjs
+++ test/spec.mjs
@@ -4,7 +4,7 @@ import docs from '../src/index.mjs'
 import css from '@magic/css'
 
 export default [
-  { fn: () => docs, expect: is.object, info: 'theme is an object' },
+  { fn: () => docs, expect: is.function, info: 'theme is a function' },
   { fn: () => css(docs), expect: is.object, info: 'theme can be parsed by css lib' },
   { fn: () => css.stringify(docs), expect: is.string, info: 'theme can be parsed by css lib' },
   {
